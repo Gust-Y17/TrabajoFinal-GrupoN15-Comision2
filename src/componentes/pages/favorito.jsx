ae5aa62a15e3dc20d6fd6c605317e5e80fec7dc2
Juan Ponzetti <juanponzetti5@example.com>
2025-06-09 21:12:30 -0300

    TP final actualizacion

---
+++ src/componentes/pages/favorito.jsx
@@ -0,0 +1,10 @@
+function Favorites() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Favoritos</h1>
+      <p>Aquí irán los productos favoritos</p>
+    </div>
+  )
+}
+
+export default Favorites
