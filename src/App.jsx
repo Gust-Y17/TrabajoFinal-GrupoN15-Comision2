9187830e41c08f82c587f7a0e89a9ea92f833771
felixxx <feli@gmail.com>
2025-06-17 03:16:25 -0300

    correciones en Nav Bar, agregado de la pagina de error, agregado de modales para la vista de detalles y editar - Leandro Felipe Quiroga

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Favorito from './componentes/favorito';
 import ProductosForm from './componentes/ProductosForm.jsx'
