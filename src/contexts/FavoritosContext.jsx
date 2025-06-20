578b887585a702b08e7fc7e1cbf79e0dd238382f
Juan Ponzetti <juanponzetti5@example.com>
2025-06-19 23:43:26 -0300

    Actualizacion N°8 Juan PonzettI

--- src/contexts/FavoritosContext.jsx
+++ src/contexts/FavoritosContext.jsx
@@ -1,19 +1,29 @@
 import {createContext, useState } from "react";
+import { useEffect } from "react";
+import { set } from "react-hook-form";
 
 export const FavoritosContext = createContext();
 
 export const FavoritoProvider = ({children}) => {
-  
-    const [favoritos, setFavoritos] = useState([]);
+    const [favoritos, setFavoritos] = useState(() =>{
+        const saved = localStorage.getItem("favoritos");
+        return saved ? JSON.parse(saved) : [];
+    });
 
+    useEffect(() => {
+        localStorage.setItem("favoritos", JSON.stringify(favoritos));
+      }, [favoritos]);
 
 const agregarFavorito = (product) => {
-    setFavoritos([...favoritos, product]);
+    if (!favoritos.some((fav) => fav.id === product.id)) {
+      setFavoritos([...favoritos, product]);
+    }
 };
 
-const eliminarFavorito = (id) => {
-    setFavoritos(favoritos.filter((product) => product.id !== id));
-  };
+const eliminarFavorito = (target) => {
+  const id = typeof target === "target" ? target.id : target;
+  setFavoritos(favoritos.filter(producto => producto.id !== id));
+};
 
 return(
     <FavoritosContext.Provider value={{favoritos, agregarFavorito, eliminarFavorito}}>{children}</FavoritosContext.Provider>
