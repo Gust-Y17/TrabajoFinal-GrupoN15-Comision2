b7edfbb184bd46987bf2394b2b9ddc04232d0bda
Zamu-ai <uriel.b.zambrano@gmail.com>
2025-06-22 14:36:41 -0300

    restriccion en los inputs ItemProducto ProductosForm y modif de prod editados en EditarProductoModal [urielzambrano]

--- src/componentes/ItemProducto.jsx
+++ src/componentes/ItemProducto.jsx
@@ -1,20 +1,39 @@
 import { useState } from "react";
 import FormularioProducto from "/src/vistas/FormularioProducto"
+ import { useContext } from 'react'
+import { ProductosContext } from '../contexts/Productos.jsx';
 
 const ItemProducto = ({ producto, onGuardar, onCancelar }) => {
-
+  const {productosAPI} = useContext(ProductosContext)
   const [prodEditado, setProdEdtado] = useState({ ...producto })
+
+  const isDuplicate = (field, value) => { 
+    return productosAPI.some(prod => 
+      prod.id !== producto.id &&  //compara id del producto actual con el que estamos editando evitando que el prod actual se considere duplicado de si mismo
+      prod[field].toLowerCase() === value.toLowerCase()
+    );
+  };
 
   const handleChange = (e) => {
     const { name, value, files } = e.target;
 
-    if (name === 'image' && files.length > 0) {
-      const file = files[0];
-      const imageURL = URL.createObjectURL(file);
-      setProdEdtado(prev => ({
-        ...prev,
-        image: imageURL
-      }));
+if (name === 'image') {
+      if (files && files.length > 0) {
+        const file = files[0];
+        const validTypes = ['image/png', 'image/jpeg', 'image/jpg'];
+        
+        if (!validTypes.includes(file.type)) { //verifica si el tipo "file.type" no está en el array validtypes que contiene las img
+          alert('Solo se permiten archivos PNG, JPEG, JPG');// muestra mensaje
+          e.target.value = ''; // limpia el valor del input y no permanezca seleccionado
+          return; //sale de la funcion evita que continue el procesamiento del formulario
+        }
+        
+        const imageURL = URL.createObjectURL(file);
+        setProdEdtado(prev => ({
+          ...prev,
+          image: imageURL
+        }));
+      }
     } else {
       setProdEdtado(prev => ({
         ...prev,
@@ -28,6 +47,15 @@ const ItemProducto = ({ producto, onGuardar, onCancelar }) => {
     const form = e.target;
     if (!form.checkValidity()) {
       form.classList.add('was-validated');
+      return;
+    }
+    if (isDuplicate('title', prodEditado.title)) {
+      alert('Ya existe un producto con ese nombre');
+      return;
+    }
+
+    if (isDuplicate('description', prodEditado.description)) {
+      alert('Ya existe un producto con esa descripción');
       return;
     }
     onGuardar(prodEditado)
